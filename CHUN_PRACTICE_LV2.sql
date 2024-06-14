667c8aed1943b438ada21c7b4d5926142495e2a9
wdh970616 <wdh970616@naver.com>
2024-06-14 09:42:01 +0900

    춘대학 Lv2

--- CHUN_PRACTICE_LV2.sql
+++ CHUN_PRACTICE_LV2.sql
@@ -30,4 +30,61 @@ select STUDENT_NO,
        year(adddate(date(substring(STUDENT_SSN, 1, 6)), interval 19 year)) 만19세되는해,
        year(date(ENTRANCE_DATE)) 입학한해
 from tb_student
-where year(adddate(date(substring(STUDENT_SSN, 1, 6)), interval 19 year)) >= year(date(ENTRANCE_DATE));
+where year(adddate(date(substring(STUDENT_SSN, 1, 6)), interval 19 year)) < year(date(ENTRANCE_DATE));
+
+#6
+select concat('2020년 크리스마스는 ', substring('일월화수목금토', dayofweek('2020-12-25'), 1), '요일이다.') as '2020년도 크리스마스의 요일';
+
+#7
+select str_to_date('99/10/11', '%y/%m/%d'),
+       str_to_date('49/10/11', '%y/%m/%d'),
+       str_to_date('70/10/11', '%y/%m/%d'),
+       str_to_date('69/10/11', '%y/%m/%d');
+
+#8
+select format(avg(POINT), 1) 평점
+from tb_grade
+where STUDENT_NO = 'A517178'
+group by STUDENT_NO;
+
+#9
+select DEPARTMENT_NO 학과번호,
+       count(*) as '학생수(명)'
+from tb_student
+group by DEPARTMENT_NO;
+
+#10
+select count(*)
+from tb_student
+group by COACH_PROFESSOR_NO
+having COACH_PROFESSOR_NO is null;
+
+#11
+select substring(term_no, 1, 4) 년도,
+       format(avg(POINT), 1) '년도별 평점'
+from tb_grade
+where STUDENT_NO = 'A112113'
+group by substring(TERM_NO, 1, 4);
+
+#12
+select department_no 학과코드명,
+       count(*) '휴학생 수'
+from tb_student
+where ABSENCE_YN = 'Y'
+group by DEPARTMENT_NO;
+
+#13
+select STUDENT_NAME 동일이름,
+       count(*) '동명인 수'
+from tb_student
+group by STUDENT_NAME
+having count(*) > 1
+order by STUDENT_NAME;
+
+#14
+select substring(TERM_NO, 1, 4) 년도,
+       substring(TERM_NO, 5, 2) 학기,
+       format(sum(POINT), 1)
+from tb_grade
+where STUDENT_NO = 'A112113'
+group by 년도, 학기;
